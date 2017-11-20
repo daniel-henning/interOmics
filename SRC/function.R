9f34e149b5788129ce658d36b5526fd5799b067b
ninghe <291784082@qq.com>
2017-11-20 22:18:51 +0800

    update and create new file

--- SRC/function.R
+++ SRC/function.R
@@ -32,60 +32,6 @@ trimming <- function(rawdat,n){
   return(t(new.dat))
 }
 
-#<<===============================================================================
-###################################clustering method##############################
-#===============================================================================>>
-########
-#1st####
-#期望最大化聚类(Expectation Maximization Algorithm)
-library(mclust)
-mc <- Mclust(input_data)
-
-########
-#2nd####
-#k-means (k-均值聚类)
-library(stats)
-kmeans.result<-kmeans(input_data,4)
-table(group_info,kmeans.result$cluster)
-
-plot(iris2[c language="("][/c],col=kmeans.result$cluster)
-# plot cluster centers
-points(kmeans.result$centers[,c(“Sepal.Length”,”Sepal.Width“)],col=1:3,pch=8,cex=2)
-########
-#3rd####
-#K-Medoids (k-中心点聚类)
-
-
-########
-#4th####
-#DBSCAN (密度聚类)
-library(cluster)#做聚类的包  
-library(fpc)#有dbscan  
-city <- read.csv("中国城市坐标.csv")  
-x <- city[,c(3,2)]#行，列  
-#ds <- dbscan(x, 2)#2是半径，最小点数默认为5  
-ds <- dbscan(x, 2,6)  
-#ds <- dbscan(x,1,3)  
-ds#可以看border，seed数  
-str(ds)#可以看列数  
-par(bg="grey")  
-plot(ds, x)  
-
-########
-#5th####
-#h-clust (层次聚类)
-#d为待处理数据集样本间的距离矩阵,可用dist()函数计算得到;
-#method参数用于选择聚类的具体算法,可供选择的有ward,single及complete等7种,默认选择complete方法;
-#参数members用于指出每个待聚类样本点/簇是由几个单样本构成,
-#该参数默认值为NULL,表示每个样本点本身即为单样本.
-#cutree()函数则可以对hclust()函数的聚类结果进行剪枝
-library(stats)
-hh<-hclust(dist(scale(input_data)),"complete")
-plot(hh,labels=w[,1],cex=0.6)
-#自动分成5类
-rect.hclust(hh,k=5)
-
-
 
 #define a function to caculate coefficient of variation
 ##################################
@@ -102,10 +48,81 @@ c.v <- function(data){
 setwd("C:\\Users\\Ning\\Desktop\\melanoma_data/")
 rawdat <- read.table('mRNA.fpkm.sorted.txt',header = T,row.names = 1,check.names = F,sep = ',')
 
-#
+
+
+
+
+
+#<<===============================================================================
+###################################clustering method##############################
+#===============================================================================>>
+########
+#1st####
+#期望最大化聚类(Expectation Maximization Algorithm)
+library(mclust)
+mc <- Mclust(as.matrix(tpm.trimmed), G=1:20)
+m.best <- dim(mc$z)[2]
+cat("model-based optimal number of clusters:", m.best, "\n")
+
+plot(d_clust)
+########
+#2nd####
+#k-means (k-均值聚类)
+library(stats)
+kmeans.result<-kmeans(tpm.trimmed,4)
+table(group_info,kmeans.result$cluster)
+
+plot(iris[,-5],col=kmeans.result$cluster)
+# plot cluster centers
+points(kmeans.result$centers[,c("Sepal.Length","Sepal.Width")],col=1:3,pch=8,cex=2)
+########
+#3rd####
+#K-Medoids (k-中心点聚类)
 library(fpc)
-pamk.result <- pamk(tpm)
+pamk.result <- pamk(tpm.trimmed)
 pamk.result$nc
+plot(pamk.result$pamobject)
+#======#
+library(cluster)
+pam.result <- pam(tpm.trimmed,3)
+table(pam.result$clustering)
+plot(pam.result)
+
+#define a function to see the results with different k-means
+wss <- (nrow(tpm.trimmed)-1)*sum(apply(tpm.trimmed,2,var))
+for(i in 2:30){wss[i] <- sum(kmeans(tpm.trimmed,centers=i)$withinss)}
+###这里的wss(within-cluster sum of squares)是组内平方和
+plot(1:20, wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
+
+########
+#4th####
+#DBSCAN (密度聚类)
+library(cluster)#做聚类的包  
+library(fpc)#有dbscan  
+#city <- read.csv("中国城市坐标.csv")  
+x <- city[,c(3,2)]#行，列  
+#ds <- dbscan(x, 2)#2是半径，最小点数默认为5  
+ds <- dbscan(tpm.trimmed, 2,6)
+#ds <- dbscan(x,1,3)  
+ds#可以看border，seed数  
+str(ds)#可以看列数  
+par(bg="grey")  
+plot(ds, tpm.trimmed)
+
+########
+#5th####
+#h-clust (层次聚类)
+#d为待处理数据集样本间的距离矩阵,可用dist()函数计算得到;
+#method参数用于选择聚类的具体算法,可供选择的有ward,single及complete等7种,默认选择complete方法;
+#参数members用于指出每个待聚类样本点/簇是由几个单样本构成,
+#该参数默认值为NULL,表示每个样本点本身即为单样本.
+#cutree()函数则可以对hclust()函数的聚类结果进行剪枝
+library(stats)
+hh<-hclust(dist(scale(input_data)),"complete")
+plot(hh,labels=w[,1],cex=0.6)
+#自动分成5类
+rect.hclust(hh,k=5)
+
 
 
 
